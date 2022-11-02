0764aaac180c43f40cf6182b5c8d4f579c2cd5b7
Karim <karim_isaac@hotmail.com>
2022-11-02 22:05:33 +0200

    Organizing Code rev02

--- Substructure Area/3-Calculation/ColumnSplit.cs
+++ Substructure Area/3-Calculation/ColumnSplit.cs
@@ -15,48 +15,58 @@ namespace Substructure_Area._3_Calculation
         private IList<Element> columnsList { get; set; }
         private List<Element> modifiedColumnsList { get; set; }
         private FormatOptions elevationformatoptions { get; set; }
-        private double eletop;
+        private double elementTopElevationValue;
 
 
         private static FilteredElementCollector ColumnsElementCollector { get; set; }
-        private List<double> elElevation { get; set; }
+        private List<double> TopelementElevationList { get; set; }
         private ForgeTypeId levelUnit { get; set; }
 
         private List<ElementId> ElementTopParameterID { get; set; }
         private List<double> ElementTopOffsetValues { get; set; }
-        private List<int> elementsindexbottom { get; set; }
-
+        private List<double> ElementbottomoffsetValues { get; set; }
+        private ICollection<ElementId> listofAlllevelsID { get; set; }
+        private List<ElementId> listofLevelsBelowUserInput { get; set; }
+        private Level bottomElementLevel { get; set; }
         private double bottomElementElevation { get; set; }
-        private double bottomoffsetValue { get; set; }
+        private double topElementElevation { get; set; }
+        
+        private int count { get; set; }
 
         public ColumnSplit(Document doc)
         {
             this.doc = doc;
             
             elevationformatoptions = doc.GetUnits().GetFormatOptions(SpecTypeId.Length);
-            elElevation = new List<double>();
+            TopelementElevationList = new List<double>();
             modifiedColumnsList = new List<Element>();
             levelUnit = elevationformatoptions.GetUnitTypeId();
             ColumnsElementCollector = new FilteredElementCollector(doc);
             columnsList = new List<Element>() ;
             ElementTopParameterID = new List<ElementId>();
+            listofAlllevelsID = new List<ElementId>();
+            listofLevelsBelowUserInput = new List<ElementId>();
             ElementTopOffsetValues = new List<double>();
+            ElementbottomoffsetValues = new List<double>();
         }
         public List<Element> columnsListChecked()
-        {
+        {   
+            //Collector for all levels
             FilteredElementCollector collector = new FilteredElementCollector(doc);
-            ICollection<ElementId> levels = collector.OfCategory(BuiltInCategory.OST_Levels)
+            listofAlllevelsID = collector.OfCategory(BuiltInCategory.OST_Levels)
                 .OfClass(typeof(Level)).ToElementIds();
 
-            List<ElementId> listoflevels = new List<ElementId>();
-            foreach (var item in levels)
+            //Level ID for the levels below User Input
+            //List<ElementId> listoflevels = new List<ElementId>();
+            foreach (var item in listofAlllevelsID)
             {
-                Level lev = doc.GetElement(item) as Level;
-                bottomElementElevation = UnitUtils.ConvertFromInternalUnits(lev.Elevation, levelUnit);
+                bottomElementLevel = doc.GetElement(item) as Level;
+                bottomElementElevation = UnitUtils.ConvertFromInternalUnits(bottomElementLevel.Elevation, levelUnit);
                 
                 if (bottomElementElevation < getLevel.Userinput)
                 {
-                    listoflevels.Add(lev.Id) ;
+
+                    listofLevelsBelowUserInput.Add(bottomElementLevel.Id) ;
                 }
             }
 
@@ -67,7 +77,7 @@ namespace Substructure_Area._3_Calculation
 
             foreach (var item in columnsList)
             {
-                foreach (var lev in listoflevels)
+                foreach (var lev in listofLevelsBelowUserInput)
                 {
                     if (item.LevelId.IntegerValue == lev.IntegerValue)
                     {
@@ -85,17 +95,22 @@ namespace Substructure_Area._3_Calculation
             ElementTopOffsetValues.AddRange(modifiedColumnsList.Select(x =>
             x.LookupParameter(LabelUtils.GetLabelFor(BuiltInParameter.SCHEDULE_TOP_LEVEL_OFFSET_PARAM))
             .AsDouble()));
-            int count = 0;
+
+            ElementbottomoffsetValues.AddRange(modifiedColumnsList.Select(x =>
+            x.LookupParameter(LabelUtils.GetLabelFor(BuiltInParameter.SCHEDULE_BASE_LEVEL_OFFSET_PARAM))
+            .AsDouble()));
+
+            count = 0;
             foreach (var item in ElementTopParameterID)
             {
                 
                 Level toplevel = doc.GetElement(item) as Level;
-                double elementtoplevel = UnitUtils.ConvertFromInternalUnits(toplevel.Elevation, levelUnit);
-                elElevation.Add(elementtoplevel + ElementTopOffsetValues.ElementAt(count));
+                topElementElevation = UnitUtils.ConvertFromInternalUnits(toplevel.Elevation, levelUnit);
+                TopelementElevationList.Add(topElementElevation + ElementTopOffsetValues.ElementAt(count));
                 count++;
             }
 
-            if (elElevation.Where(x => x > getLevel.Userinput).Any())
+            if (TopelementElevationList.Where(x => x > getLevel.Userinput).Any())
             {
                 switch (MessageBox.Show("Top level for some columns are above the entered level. " +
                     "Would you Like to split them at the entered level?", "Columns Top Level",
@@ -103,157 +118,56 @@ namespace Substructure_Area._3_Calculation
 
                 {
                     case MessageBoxResult.Yes:
-
+                        ElementTopParameterID.Clear();
+                        count = 0;
                         foreach (var ele in modifiedColumnsList)
                         {
-
+                            
                             FamilyInstance column = ele as FamilyInstance;
 
-                            ElementId topLevelParameter = column
+                            ElementTopParameterID.Add(column
                                 .LookupParameter(LabelUtils.GetLabelFor(BuiltInParameter.SCHEDULE_TOP_LEVEL_PARAM))
-                                .AsElementId();
-                            Level tolevel = doc.GetElement(topLevelParameter) as Level;
-                            eletop = UnitUtils.ConvertFromInternalUnits(tolevel.Elevation, levelUnit);
+                                .AsElementId());
+                            Level tolevel = doc.GetElement(ElementTopParameterID.ElementAt(count)) as Level;
+                            elementTopElevationValue = UnitUtils.ConvertFromInternalUnits(tolevel.Elevation, levelUnit);
 
                             ElementId bottomlevelpara = column
                             .LookupParameter(LabelUtils.GetLabelFor(BuiltInParameter.SCHEDULE_BASE_LEVEL_PARAM))
                             .AsElementId();
                             Level bottomlevel = doc.GetElement(bottomlevelpara) as Level;
-                            double elebottom = UnitUtils.ConvertFromInternalUnits(bottomlevel.Elevation, levelUnit);
+                            bottomElementElevation = UnitUtils.ConvertFromInternalUnits(bottomlevel.Elevation, levelUnit);
 
                             double topoffsetvalue = column
                             .LookupParameter(LabelUtils.GetLabelFor(BuiltInParameter.SCHEDULE_TOP_LEVEL_OFFSET_PARAM))
                             .AsDouble();
 
 
-                            double elementlength = (getLevel.Userinput - elebottom)
+                            double splitRatio = (getLevel.Userinput - elebottom)
                                                     / (eletop + topoffsetvalue - elebottom);
                             if (eletop + topoffsetvalue > getLevel.Userinput)
                             {
                                 using (Transaction tran = new Transaction(doc, "Split Columns"))
                                 {
                                     tran.Start();
-                                    column.Split(elementlength);
+                                    column.Split(splitRatio);
                                     tran.Commit();
                                 }
                             }
-                            
-
-
-                            //modifiedColumnsList.Add(column);
-
-
-
-
+                            count++;
                         }
                         break;
                 }
-
             }
-            else if (elElevation.Where(x => x < getLevel.Userinput).Any())
+            else if (TopelementElevationList.Where(x => x < getLevel.Userinput).Any())
             {
                 TaskDialog.Show("Split Columns", "Columns top level is equal to the top elevation level");
             }
+     
             return modifiedColumnsList;
+            
         }
    
     }
-}            
-
-
-
-
-
-
-        //MessageBoxResult result = (MessageBox.Show("Top level for some columns are above the entered level. " +
-        //            "Would you Like to split them at the entered level?", "Columns Top Level",
-        //            MessageBoxButton.YesNo, MessageBoxImage.Question));
-        //        if (result == MessageBoxResult.OK)
-        //        {
-        //            foreach (Element ele in columnsList)
-        //            {
-        //                FamilyInstance column = ele as FamilyInstance;
-        //                ElementId topLevelParameter = column
-        //                    .LookupParameter(LabelUtils.GetLabelFor(BuiltInParameter.SCHEDULE_TOP_LEVEL_PARAM))
-        //                    .AsElementId();
-        //                Level toplevel = doc.GetElement(topLevelParameter) as Level;
-        //                elElevation = UnitUtils.ConvertFromInternalUnits(toplevel.Elevation, levelUnit);
-        //                ElementId bottomlevelpara = column
-        //                 .LookupParameter(LabelUtils.GetLabelFor(BuiltInParameter.SCHEDULE_BASE_LEVEL_PARAM))
-        //                 .AsElementId();
-        //        Level bottomlevel = doc.GetElement(bottomlevelpara) as Level;
-        //        double elementlength = (getLevel.Userinput - bottomlevel.Elevation)
-        //                                / (toplevel.Elevation - bottomlevel.Elevation);
-
-        //        using (Transaction tran = new Transaction(doc, "splitcolumn"))
-        //        {
-        //            tran.Start();
-        //            column.Split(elementlength);
-        //            tran.Commit();
-        //        }
-        //        modifiedColumnsList.Add(column);
-        //break;
-
-
-
-        //    FamilyInstance column = ele as FamilyInstance;
-        //    ElementId topLevelParameter = column
-        //        .LookupParameter(LabelUtils.GetLabelFor(BuiltInParameter.SCHEDULE_TOP_LEVEL_PARAM))
-        //        .AsElementId();
-        //    Level toplevel = doc.GetElement(topLevelParameter) as Level;
-        //    elElevation = UnitUtils.ConvertFromInternalUnits(toplevel.Elevation, levelUnit);
-        //    if (elElevation > getLevel.Userinput)
-        //    {
-        //        switch (MessageBox.Show("Top level for some columns are above the entered level. " +
-        //            "Would you Like to split them at the entered level?", "Columns Top Level",
-        //            MessageBoxButton.YesNo, MessageBoxImage.Question))
-        //        {
-        //            case MessageBoxResult.Yes:
-        //                //splitcolumn(column, ele, toplevel);
-        //                ElementId bottomlevelpara = column
-        //                .LookupParameter(LabelUtils.GetLabelFor(BuiltInParameter.SCHEDULE_BASE_LEVEL_PARAM))
-        //                .AsElementId();
-        //                Level bottomlevel = doc.GetElement(bottomlevelpara) as Level;
-        //                double elementlength = (getLevel.Userinput - bottomlevel.Elevation)
-        //                                        / (toplevel.Elevation - bottomlevel.Elevation);
-
-        //                using (Transaction tran = new Transaction(doc, "splitcolumn"))
-        //                {
-        //                    tran.Start();
-        //                    column.Split(elementlength);
-        //                    tran.Commit();
-        //                }
-        //                modifiedColumnsList.Add(column);
-
-
-        //                break;
-        //            case MessageBoxResult.No:
-        //                break;
-
-
-
-        //            }
-
-        //        }
-        //        return modifiedColumnsList;
-        //    }
-
-        //}
-        //public void splitcolumn(FamilyInstance column, Element ele, Level toplevel)
-        //{
-        //    ElementId bottomlevelpara = column
-        //    .LookupParameter(LabelUtils.GetLabelFor(BuiltInParameter.SCHEDULE_BASE_LEVEL_PARAM))
-        //    .AsElementId();
-        //    Level bottomlevel = doc.GetElement(bottomlevelpara) as Level;
-        //    double elementlength = (getLevel.Userinput - bottomlevel.Elevation)
-        //                            / (toplevel.Elevation - bottomlevel.Elevation);
-
-        //    using (Transaction tran = new Transaction(doc, "splitcolumn"))
-        //    {
-        //        tran.Start();
-        //        column.Split(elementlength);
-        //        tran.Commit();
-        //    }
-        //    modifiedColumnsList.Add(column);
+}    
 
 
