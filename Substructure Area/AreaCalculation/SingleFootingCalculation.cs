c52963157b5ef97cb574f82f0b7f4cd6970c61e0
Karim <karim_isaac@hotmail.com>
2022-11-16 22:07:36 +0200

    Footings and strip and isolated are organised and code is working

--- Substructure Area/AreaCalculation/SingleFootingCalculation.cs
+++ Substructure Area/AreaCalculation/SingleFootingCalculation.cs
@@ -14,13 +14,16 @@ namespace Substructure_Area
 {
     public class SingleFootingCalculation
     {
-        public string faceInfo { get; set; }
-        public Element ele { get; set; }
-        public int faces { get; set; }
-        public double totalArea { get; set; }
-        public DataTable table { get; set; }
-        public DataColumn header { get; set; }
-        public static List<string> infofaces { get; set; }
+        private string faceInfo { get; set; }
+        private Element ele { get; set; }
+        private int faces { get; set; }
+        private double totalArea { get; set; }
+        private DataTable table { get; set; }
+        private DataColumn header { get; set; }
+        private DataColumn header2 { get; set; }
+        private DataRow rowData { get; set; }
+        private static List<string> infofaces { get; set; }
+        private GeometryInstance geoInst { get; set; }
         public SingleFootingCalculation()
 
         {
@@ -35,12 +38,13 @@ namespace Substructure_Area
             table = new DataTable();
 
             this.ele = ele;
-            header = new DataColumn(ele.Name);
-            
+            header = new DataColumn("Faces");
+            header2 = new DataColumn(ele.Name);
             table.Columns.Add(header);
+            table.Columns.Add(header2);
             foreach (GeometryObject geomObj in geoElem)
             {
-                GeometryInstance geoInst = geomObj as GeometryInstance;
+                geoInst = geomObj as GeometryInstance;
                 //isolated footing data
                 if (null != geoInst)
                 {
@@ -48,26 +52,36 @@ namespace Substructure_Area
                     {
                         if (null != geoSolid && geoSolid.Id != -1)
                         {
+                            faces = 0;
                             foreach (Face geomFace in geoSolid.Faces)
                             {
                                 //for (int i = 0; i < faces; i++)
                                 {
-                                    faces++;
-                                    faceInfo += "Face " + faces + " area: " + UnitUtils.ConvertFromInternalUnits(geomFace.Area, areaUnit).ToString() + "\n";
-                                    infofaces.Add(UnitUtils.ConvertFromInternalUnits(geomFace.Area, areaUnit).ToString());
-                                    table.Rows.Add(UnitUtils.ConvertFromInternalUnits(geomFace.Area, areaUnit));
                                     
-                                    totalArea += geomFace.Area;
+                                    faces++;
+                                    rowData = table.NewRow();
+                                    rowData[header] = faces;
+                                    rowData[header2] = UnitUtils.ConvertFromInternalUnits(geomFace.Area, areaUnit);
+                                    table.Rows.Add(rowData);
+                                    //faceInfo += "Face " + faces + " area: " + UnitUtils.ConvertFromInternalUnits(geomFace.Area, areaUnit).ToString() + "\n";
+                                    //infofaces.Add(UnitUtils.ConvertFromInternalUnits(geomFace.Area, areaUnit).ToString());
+                                    //table.Rows.Add(UnitUtils.ConvertFromInternalUnits(geomFace.Area, areaUnit));
+                                    
+                                    //totalArea += geomFace.Area;
                                 }
                                 
                             }
-                                var result = table.AsEnumerable().Sum(x => Convert.ToDouble(x[ele.Name]));
-                                table.Rows.Add(result);
-                            
-                        
 
-                            faceInfo += "Number of faces: " + faces + "\n";
-                            faceInfo += "Total area: " + UnitUtils.ConvertFromInternalUnits(totalArea, areaUnit).ToString() + "\n";
+                            double result = table.AsEnumerable().Sum(x => Convert.ToDouble(x[ele.Name]));
+                            rowData = table.NewRow();
+                            rowData[header] = "Total";
+                            rowData[header2] = result;
+                            table.Rows.Add(rowData);
+
+
+
+                            //faceInfo += "Number of faces: " + faces + "\n";
+                            //faceInfo += "Total area: " + UnitUtils.ConvertFromInternalUnits(totalArea, areaUnit).ToString() + "\n";
                             
 
                         }
@@ -80,24 +94,36 @@ namespace Substructure_Area
                     Solid geoSolid = geomObj as Solid;
                     if (null != geoSolid)
                     {
+                        faces = 0;
                         foreach (Face geoface in geoSolid.Faces)
                         {
                             if (null != geoface && geoface.Id != -1)
                             {
-                                infofaces.Add(UnitUtils.ConvertFromInternalUnits(geoface.Area, areaUnit).ToString());
+                                //infofaces.Add(UnitUtils.ConvertFromInternalUnits(geoface.Area, areaUnit).ToString());
+                                
                                 faces++;
-                                faceInfo += "Face " + faces + " area: " + UnitUtils.ConvertFromInternalUnits(geoface.Area, areaUnit).ToString() + "\n";
+                                
+                                rowData = table.NewRow();
+                                rowData[header] = faces;
+                                rowData[header2] = UnitUtils.ConvertFromInternalUnits(geoface.Area, areaUnit);
+                                table.Rows.Add(rowData);
+                                //faceInfo += "Face " + faces + " area: " + UnitUtils.ConvertFromInternalUnits(geoface.Area, areaUnit).ToString() + "\n";
 
-                                totalArea += geoface.Area;
+                                //totalArea += geoface.Area;
 
                                 //faceInfo += "Number of faces: " + faces + "\n";
-                                header = new DataColumn("Face" + faces, typeof(double));
+                                //header = new DataColumn("Face" + faces, typeof(double));
                                 //table.Columns.Add(header);
-                                table.Rows.Add(UnitUtils.ConvertFromInternalUnits(geoface.Area, areaUnit));
-                                faceInfo += "Total area: " + UnitUtils.ConvertFromInternalUnits(totalArea, areaUnit).ToString() + "\n";
+                                //table.Rows.Add(UnitUtils.ConvertFromInternalUnits(geoface.Area, areaUnit));
+                                //faceInfo += "Total area: " + UnitUtils.ConvertFromInternalUnits(totalArea, areaUnit).ToString() + "\n";
                             }
                         }
-                        faceInfo += "Number of faces: " + faces + "\n";
+                        //faceInfo += "Number of faces: " + faces + "\n";
+                        double result = table.AsEnumerable().Sum(x => Convert.ToDouble(x[ele.Name]));
+                        rowData = table.NewRow();
+                        rowData[header] = "Total";
+                        rowData[header2] = result;
+                        table.Rows.Add(rowData);
                     }
                 }
             }
