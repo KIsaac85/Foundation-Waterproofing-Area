f8f0f3fd426db6b20e7b43014b67c56e08547b4c
Karim <karim_isaac@hotmail.com>
2022-11-18 22:48:42 +0200

    unused package is deleted and the result issue is resolved

--- Substructure Area/AreaCalculation/SingleFootingCalculation.cs
+++ Substructure Area/AreaCalculation/SingleFootingCalculation.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Caliburn.Micro;
+ 
 
 
 
@@ -14,21 +14,18 @@ namespace Substructure_Area
 {
     public class SingleFootingCalculation
     {
-        private string faceInfo { get; set; }
-        private Element ele { get; set; }
+
+        
         private int faces { get; set; }
-        private double totalArea { get; set; }
+        
         private DataTable table { get; set; }
         private DataColumn header { get; set; }
         private DataColumn header2 { get; set; }
         private DataRow rowData { get; set; }
-        private static List<string> infofaces { get; set; }
-        private GeometryInstance geoInst { get; set; }
-        public SingleFootingCalculation()
 
-        {
-            infofaces = new List<string>();
-        }
+        private GeometryInstance geoInst { get; set; }
+        private double result { get; set; }
+        
         
         public DataTable faceinfor(Element ele, GeometryElement geoElem, Document doc)
         {
@@ -37,12 +34,12 @@ namespace Substructure_Area
             ForgeTypeId areaUnit = areaFormatOptions.GetUnitTypeId();
             table = new DataTable();
 
-            this.ele = ele;
+            
             header = new DataColumn("Faces");
             header2 = new DataColumn(ele.Name);
             table.Columns.Add(header);
             table.Columns.Add(header2);
-            int count=0;
+            
             //isolated footing data
             foreach (GeometryObject geomObj in geoElem)
             {
@@ -51,27 +48,31 @@ namespace Substructure_Area
                
                 if (null != geoInst)
                 {
-                    count++;
+                    
                     foreach (Solid geoSolid in geoInst.SymbolGeometry)
                     {
-                        if (null != geoSolid && geoSolid.Id != -1)
+                        if (null != geoSolid)
                         {
                             faces = 0;
                             foreach (Face geomFace in geoSolid.Faces)
                             {
-                                {
-                                    faces++;
-                                    rowData = table.NewRow();
-                                    rowData[header] = faces;
-                                    rowData[header2] = Math.Round(UnitUtils.ConvertFromInternalUnits(geomFace.Area, areaUnit),2);
-                                    table.Rows.Add(rowData);
-                                }
+                                
+                                faces++;
+                                rowData = table.NewRow();
+                                rowData[header] = faces;
+                                rowData[header2] = Math.Round(UnitUtils.ConvertFromInternalUnits(geomFace.Area, areaUnit),2);
+                                table.Rows.Add(rowData);
+                                
                             }
-                            double result = table.AsEnumerable().Sum(x => Convert.ToDouble(x[ele.Name]));
-                            rowData = table.NewRow();
-                            rowData[header] = "Total";
-                            rowData[header2] = result;
-                            table.Rows.Add(rowData);
+                            if (geoSolid.SurfaceArea != 0)
+                            {
+                                result = Math.Round(UnitUtils.ConvertFromInternalUnits(geoSolid.SurfaceArea, areaUnit), 2);
+                                rowData = table.NewRow();
+                                rowData[header] = "Total";
+                                rowData[header2] = result;
+                                table.Rows.Add(rowData);
+                            }
+                                
                         }
                     }
                 }
@@ -80,7 +81,7 @@ namespace Substructure_Area
                 else if(null == geoInst)
                 {
                     Solid geoSolid = geomObj as Solid;
-                    if (null != geoSolid&&geoSolid.Id!=-1)
+                    if (null != geoSolid)
                     {
                         faces = 0;
                         foreach (Face geoface in geoSolid.Faces)
@@ -94,11 +95,15 @@ namespace Substructure_Area
                                 table.Rows.Add(rowData);
                             }
                         }
-                        double result = table.AsEnumerable().Sum(x => Convert.ToDouble(x[ele.Name]));
-                        rowData = table.NewRow();
-                        rowData[header] = "Total";
-                        rowData[header2] = result;
-                        table.Rows.Add(rowData);
+                        if (geoSolid.SurfaceArea != 0)
+                        {
+                            result = Math.Round(UnitUtils.ConvertFromInternalUnits(geoSolid.SurfaceArea, areaUnit), 2);
+                            rowData = table.NewRow();
+                            rowData[header] = "Total";
+                            rowData[header2] = result;
+                            table.Rows.Add(rowData);
+                        }
+                        
                     }
                 }
             }
