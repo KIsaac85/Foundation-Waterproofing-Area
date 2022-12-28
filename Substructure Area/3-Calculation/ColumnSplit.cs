67ad41be78ae92309251d06682d0756e86c5ceb4
Karim <karim_isaac@hotmail.com>
2022-12-28 21:35:19 +0200

    column split is working count++ was missing and added

--- Substructure Area/3-Calculation/ColumnSplit.cs
+++ Substructure Area/3-Calculation/ColumnSplit.cs
@@ -176,6 +176,7 @@ namespace Substructure_Area._3_Calculation
  
                                 }
                             }
+                            count++;
                             
                         }
                         break;
